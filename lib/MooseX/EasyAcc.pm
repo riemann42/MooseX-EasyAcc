3d88a6796aedaafdd6c0fb80349b752dfba75886
Edward Allen <ealleniii@cpan.org>
2011-06-12 22:55:26 -0700

    First version of module

--- lib/MooseX/EasyAcc.pm
+++ lib/MooseX/EasyAcc.pm
@@ -1,9 +1,96 @@
-use strict;
-use warnings;
 package MooseX::EasyAcc;
 
+#ABSTRACT: Name your accessors foo(), set_foo(), and has_foo()
+use Moose 1.9900;
+use Moose::Exporter;
 
-1;
+Moose::Exporter->setup_import_methods(
+    class_metaroles => {
+        attribute => ['MooseX::EasyAcc::Role::Attribute'],
+
+    },
+    role_metaroles =>
+        { applied_attribute => ['MooseX::EasyAcc::Role::Attribute'], },
+
+);
+
+1;    # Magic true value required at end of module
+__END__
 
 
+=head1 SYNOPSIS
 
+    package MyApp;
+    use Moose;
+    use MooseX::EasyAcc;
+
+    has 'name' => (
+        is => 'rw',
+        isa => 'Str'
+    );
+    # This creates methods: name, set_name, has_name
+
+    has 'supernumber' => (
+        is => 'ro',
+        isa => 'Int',
+    );
+    # This creates methods: supernumber, _set_supernumber, has_supernumber
+
+    has '_superhero_name' => (
+        is => 'ro',
+        isa => 'Str'
+    );
+    # This creates methods: _superhero_name, _set__superhero_name, _has__superhero_name
+
+    has 'superpower' => (
+        is => 'ro',
+        isa => 'Str',
+        predicate => 'is_awesome',
+    );
+    # This creates methods: superpower, _set_supernumber, is_awesome
+   
+    has 'nemisis' => (
+        is => 'rw',
+        isa => 'MyApp',
+        predicate => 'is_loved',
+        reader => 'best_friend',
+    );
+    # This creates methods: best_friend , set_nemisis, is_loved
+    # and so on....
+    
+  
+=head1 DESCRIPTION
+
+Like L<MooseX::FollowPBP>, or L<MooseX::SemiAffordanceAccessor> this changes the naming convention for accessors.
+
+Attributes are created using L<SemiAffordance|MooseX::SemiAffordanceAccessor>
+style (attr, set_attr) with the addition of a predicate (prefixed with 'has_').  Unlike
+L<MooseX::SemiAffordanceAccessor>, setting 'ro' does not prevent the creation
+of a writer, only makes it private (by prepending a '_').  Also, explicitly
+setting an accessor only overwrites this accessor, doesn't cancel the whole
+scheme.
+
+In the case of an attribute starting with underscore, '_set_' and '_has_'
+prefix is used.
+
+=head1 NOTE ON USAGE IN ROLES
+
+Applying this module to a class will not affect the default attribute traits
+of roles assigned to the class.
+
+Applying this module to a role will affect all attributes in that role, but
+not classes that it is applied to.
+
+=head1 ACKNOWLEDGMENTS
+
+Much of this is based on reviewing Dave Rolsky's MooseX::FollowPBP.  This
+module is Copyright (c) 2011 by Dave Rolsky and is available wherever fine
+Perl modules are distributed.
+
+=head1 SEE ALSO
+Moose
+MooseX::FollowPBP
+MooseX::SemiAffordanceAccessor
+
+=for stopwords
+Rolsky Rolsky's SemiAffordance attr ro
---
+++ lib/MooseX/EasyAcc/Role/Attribute.pm
@@ -0,0 +1,43 @@
+package MooseX::EasyAcc::Role::Attribute;
+#ABSTRACT: Attribute trait for L<MooseX::EasyAcc>
+use Moose::Role;
+use Data::Dumper;
+before _process_options => sub {
+    my ($class, $name,$options) = @_;
+
+
+    # This is based on MooseX::FollowPBP::Role::Attribute .. loosely.
+    if ( ( exists $options->{is} ) && ( $options->{is} ne 'bare' ) ) {
+        # Everything gets a predicate
+        if ( ! exists $options->{predicate} ) {
+            my $has = ( $name =~ m{^_} ) ?  '_has_'
+                                         :  'has_';
+            $options->{predicate} = $has . $name;
+        }
+        # Everything gets a reader (SemiAffordable style... 
+        #   objects have things, you don't get things!)
+        if (( ! exists $options->{reader}  ) || (! $options->{reader})) {
+            $options->{reader} = $name;
+        }
+        # And finally, everything, even ro, gets a writer.
+        # TODO : create a writer that checks who you are, making it truly private.
+        if ( ! exists $options->{writer} ) {
+            my $set = (( $name =~ m{^_} ) || ($options->{is} eq 'ro')) ?  '_set_'
+                                                                       :  'set_';
+            $options->{writer} = $set . $name;
+        }
+        delete $options->{is};
+    }
+};
+
+1;
+
+
+=head1 SYNOPSIS
+
+See L<MooseX::EasyAcc>
+  
+=head1 DESCRIPTION
+
+This is the trait that is applied to attributes.
+
